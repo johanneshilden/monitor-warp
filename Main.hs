2236c23ae113650c47f22eec51be7a8ea63978ba
johanneshilden <hildenjohannes@gmail.com>
2014-09-12 18:01:06 +0200

    code cleanup

--- Main.hs
+++ Main.hs
@@ -3,27 +3,36 @@ module Main where
 
 import Control.Concurrent                ( threadDelay )
 import Monitor.Warp
+import Data.ByteString.Lazy              ( ByteString )
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp
 
-app :: Application
-app req resp = do
-    threadDelay 5000000 -- Sleep for 5 secs.
-    resp $ responseLBS ok200 [] "YOLO!\n"
+import qualified Data.ByteString.Lazy.Char8 as BL
 
-onStartup :: IO (ServerSettings ())
+data AppData = AppData ByteString
+
+app :: AppData -> Application
+app (AppData a) req resp = do
+    threadDelay 2000000 -- Sleep for 2 secs.
+    resp $ responseLBS ok200 [] $ BL.concat [a, " says: YOLO!\n"]
+
+onStartup :: IO (ServerSettings AppData)
 onStartup = do
+    putStrLn "Service starting."
     return $ ServerSettings 
         { handler  = app
         , config   = setPort 3333 $ defaultSettings
-        , response = responseLBS serviceUnavailable503 [] "Shutting down."
-        , options  = ()
+        , response = responseLBS serviceUnavailable503 [] "Server shutting down."
+        , options  = AppData "demo"
         }
 
 -- | Do nothing in particular.
-onExit :: ServerSettings () -> IO ()
-onExit _ = return ()
+onExit :: ServerSettings AppData -> Shutdown -> IO ()
+onExit _ action = do
+    case action of
+        Restart -> putStrLn "Service closing down for restart.\n"
+        Exit    -> putStrLn "Service terminating.\n"
 
 main :: IO ()
 main = sigServ onStartup onExit
--- Monitor/Warp.hs
+++ Monitor/Warp.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE RecordWildCards #-}
-module Monitor.Warp where
+module Monitor.Warp 
+    ( sigServ
+    , ServerSettings(..)
+    , Shutdown(..)
+    ) where
 
-import Control.Concurrent                ( forkIO, killThread )
+import Control.Concurrent                ( forkIO, killThread, threadDelay )
 import Control.Concurrent.STM
 import Control.Monad                     ( when, liftM, join, void )
 import Control.Monad.Trans               ( liftIO )
@@ -14,32 +18,35 @@ data Shutdown = Exit | Restart
 
 -- | This function "wraps" around the original request handler to allow 
 -- graceful shutdown and restart operations.
-proxy :: TMVar Shutdown -> Application -> Response -> Application
-proxy shutdown handler message req resp = 
+proxy :: a -> TMVar Shutdown -> (a -> Application) -> Response -> Application
+proxy options shutdown handler message req resp = 
     join . atomically $ do
         -- Check if a shutdown/restart has been initiated
         shouldRun <- isEmptyTMVar shutdown
         return $ if shouldRun
-            then handler req resp
+            then (handler options) req resp
             -- If the TMVar is full, we're shutting down
             else resp message
 
--- | Server configuration parameterized over a generic type. This can be used 
+-- | Server configuration parameterized over a generic type which can be used 
 -- by client applications to supply the request handler with shared application 
 -- data, e.g., a database connection handle. 
 data ServerSettings a = ServerSettings
-    { handler  :: Application   -- ^ The client's request handler
-    , config   :: Settings      -- ^ Warp server settings
-    , response :: Response      -- ^ A response to return during shutdown
-    , options  :: a             -- ^ Client application data which should be
-                               -- made available to the request handler.
+    { handler  :: a -> Application  -- ^ Client's request handler
+    , config   :: Settings         -- ^ Warp server settings
+    , response :: Response         -- ^ A response to return during shutdown
+    , options  :: a                -- ^ Client application data which should be
+                                  -- made available to the request handler.
     }
 
--- | Provides some process management capabilities via SIGHUP and SIGTERM to the 
--- Warp server by running the server as a monitored worker thread and installing 
--- the required signal handlers.
-sigServ :: IO (ServerSettings a)      -- ^ Called prior to server start
-        -> (ServerSettings a -> IO ()) -- ^ Shutdown hook
+-- | Adds some process management capabilities via SIGHUP and SIGTERM to the 
+-- Warp server by running the server as a monitored worker thread and installing
+-- the necessary signal handlers.
+sigServ :: IO (ServerSettings a)      
+        -- ^ Called prior to server start
+        -> (ServerSettings a -> Shutdown -> IO ()) 
+        -- ^ Shutdown hook. Accepts the settings object and a value to indicate
+        -- the type of shutdown operation (exit or restart).
         -> IO ()
 sigServ onStartup onExit = do
 
@@ -50,8 +57,6 @@ sigServ onStartup onExit = do
     installHandler sigTERM (Catch $ term shutdown) (Just fullSignalSet)
 
     runWorker shutdown conns 
-
-    putStrLn "-- Shutting down. --"
 
   where
     runWorker :: TMVar Shutdown -> TVar Int -> IO ()
@@ -64,17 +69,19 @@ sigServ onStartup onExit = do
                       $ setOnClose (onClose conns)
                         config
 
-        worker <- forkIO $ runSettings settings' (proxy shutdown handler response)
+        worker <- forkIO $ runSettings settings' (proxy options shutdown handler response)
         action <- monitor shutdown conns
 
         -- The worker is taken down, in case we intend to restart the service
         killThread worker
 
-        onExit settings
+        onExit settings action
     
         -- If a SIGHUP was received, we reload any configuration
         -- files, re-initialize the server and fork a new thread
-        when (Restart == action) $ runWorker shutdown conns 
+        when (Restart == action) $ do
+            threadDelay 2000 -- Sleep for 0.002 secs.
+            runWorker shutdown conns 
 
 -- | Wait for the worker thread to signal exit via the TMVar synchronization 
 -- primitive and then block until all outstanding requests have completed.
